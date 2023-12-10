b1173d02679b7ab94f6b2914b633a43ba16708da
as7haro7 <erickbreack72@gmail.com>
2023-12-09 23:44:05 -0400

    react bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import NavbarRes from "./pages/header";
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <NavbarRes></NavbarRes>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Carusel.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const caruse = () => {
+  return (
+    <div>caruse</div>
+  )
+}
