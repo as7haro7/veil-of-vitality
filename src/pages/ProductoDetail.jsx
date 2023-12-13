be34d5a9106ad1e079c8fac8b8bc023af071c6af
as7haro7 <erickbreack72@gmail.com>
2023-12-12 21:26:05 -0400

    detalleCard centrado

--- src/pages/ProductoDetail.jsx
+++ src/pages/ProductoDetail.jsx
@@ -5,20 +5,21 @@ import Button from 'react-bootstrap/Button';
 const DetalleProducto = ({ show, onClose, title, description, price, imageSrc }) => {
   return (
     <Modal show={show} onHide={onClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>{title}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <img src={imageSrc} alt={title} className="img-fluid" />
-        <p>{description}</p>
-        <p>Precio: {price}</p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="outline-dark" onClick={onClose}>
-          Cerrar
-        </Button>
-      </Modal.Footer>
-    </Modal>
+    <Modal.Header closeButton>
+      <Modal.Title>{title}</Modal.Title>
+    </Modal.Header>
+    <Modal.Body className="text-center">
+      <img src={imageSrc} alt={title} className="img-fluid mx-auto d-block" />
+      <p>{description}</p>
+      <p>Precio: {price}</p>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant="outline-dark" onClick={onClose}>
+        Cerrar
+      </Button>
+    </Modal.Footer>
+  </Modal>
+  
   );
 };
 
